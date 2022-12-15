b934201748200edacbcada4e8e08eb4aeaca2128
PioneerX7 <vita-kopy@yandex.ru>
2022-12-15 15:16:31 +0300

    tasks 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+
+        int moneyForMonth = 15000;
+        int total = 0;
+        int totalMonth = 0;
+
+        while (total < 2_459_000) {
+            total = total + moneyForMonth;
+            totalMonth++;
+
+        }
+        System.out.println("Месяц " + totalMonth + ", сумма накоплений равна " + total + " рублей");
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+
+        int peopleY = 12_000_000;
+        int fertilityFor1000 = 17;
+        int mortalityFor1000 = 8;
+        int totalYear = 0;
+
+        while (totalYear < 10) {
+            totalYear++;
+            peopleY = peopleY + (peopleY / 1000 * (fertilityFor1000 - mortalityFor1000));
+
+            System.out.println("Год " + totalYear + ", численность населения составляет " + peopleY);
+        }
     }
 }
